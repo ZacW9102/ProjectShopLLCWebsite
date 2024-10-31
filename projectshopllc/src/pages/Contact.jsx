fe4f6dc746e054d604e91bbd19a34969736ec577
ZacW9102 <zacw9102@gmail.com>
2024-10-31 14:01:03 -0700

    update contact information

--- projectshopllc/src/pages/Contact.jsx
+++ projectshopllc/src/pages/Contact.jsx
@@ -19,13 +19,11 @@ export default function Contact() {
                 <div className="contact">
                     <h3>Eve Webb</h3>
                     <p onClick={handleCopy}>602-390-2140 direct</p>
-                    <p onClick={handleCopy}>602-926-2659 efax </p>
                     <p onClick={handleCopy}>eve.webb@projectshopllc.com</p>
                 </div>
                 <div className="contact">
                     <h3>Tricia Rochford</h3>
                     <p onClick={handleCopy}>602-390-6642 direct</p>
-                    <p onClick={handleCopy}>602-926-2659 efax</p>
                     <p onClick={handleCopy}>tricia.rochford@projectshopllc.com</p>
                 </div>
             </div>
